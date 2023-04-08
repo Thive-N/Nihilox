852eb7695c0c806f4964ce2bdfb5eeef707161f8
Thivejan Neranjan <35178093+Thive-N@users.noreply.github.com>
2023-04-09 00:15:20 +0100

    adding a lexer test
    added a lexer test so that all main parts of the code is tested and working instead of going off assumptions

---
+++ test/lexerTest.cpp
@@ -0,0 +1,26 @@
+#include <utility>
+
+#include "gtest/gtest.h"
+#include "../src/lexer.h"
+
+TEST (BasicTests, LexerTest) {
+	auto expressions = new std::unordered_map<std::string, std::string*>();
+	expressions->insert({"1 + 2 * 3;", new std::string[5]{"1", "+", "2", "*", "3"}});
+	expressions->insert({"1 * 2 + 3;", new std::string[5]{"1", "*", "2", "+", "3"}});
+	expressions->insert({"1 / 2 + 3;", new std::string[5]{"1", "/", "2", "+", "3"}});
+	expressions->insert({"1 == 2 + 3;", new std::string[5]{"1", "==", "2", "+", "3"}});
+	expressions->insert({"1 + 2 == 3;", new std::string[5]{"1", "+", "2", "==", "3"}});
+	expressions->insert({"1 + 2 * 3 == 4;", new std::string[7]{"1", "+", "2", "*", "3", "==", "4"}});
+	expressions->insert({"1 * 2 + 3 == 4;", new std::string[7]{"1", "*", "2", "+", "3", "==", "4"}});
+
+	for (auto &expr : *expressions) {
+		int size;
+		std::string *tokens = lex(expr.first, size);
+		for (int i = 0; i < expr.second->size(); i++) {
+			if (i>=size)
+				FAIL() << "Expected " << expr.second->size() << " tokens, got " << size;
+			ASSERT_EQ(tokens[i], expr.second[i]);
+		}
+	}
+
+}
