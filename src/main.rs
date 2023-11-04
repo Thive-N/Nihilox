41f624b96838b8d8fa99c09552a8ec26c2d7f910
Thive-N <35178093+Thive-N@users.noreply.github.com>
2023-11-04 22:34:24 +0000

    Coverage reports
    added a github workflow for coverage reports

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
 }
+
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
+    }
+}
